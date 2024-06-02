2904014f830ed99d4f353e4581c3c32cc1d26546
Icy-Bear <prakhar.aka.nb@gmail.com>
2024-06-02 10:15:39 +0530

    README.md Updated :)

--- Linear_Regression/Linear_Regression_FS.py
+++ Linear_Regression/Linear_Regression_FS.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
 
 from sklearn.datasets import load_iris 
 from sklearn.model_selection import train_test_split
@@ -10,8 +9,34 @@ iris = load_iris()
 X = iris.data 
 Y = iris.target
 
-x_train, x_test, y_trian, y_test = train_test_split(X, Y, test_size = 0.2) 
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2) 
 
+x_train = np.vstack((np.ones((x_train.shape[0],)), x_train.T)).T
+x_test = np.vstack((np.ones((x_test.shape[0],)), x_test.T)).T
 
+print(x_train.shape)
+print(x_test.shape)
+
+def Linear_Regression(x_train, y_train, learning_rate, iteration):
+    m = y_train.size
+    theta = np.zeros((x_train.shape[1],1))
+
+    for i in range(iteration):
+        y_pred = np.dot(x_train, theta)
+        cost = ( 1/(2*m) ) * np.sum(np.square( y_pred - y_train ))
+        d_theta = (1/m) * np.dot(x_train.T, y_pred - y_train )
+        theta = theta - learning_rate * d_theta
+
+    return theta
+
+iteration = 10000
+learning_rate = 0.000000005
+theta = Linear_Regression(x_train, y_train, learning_rate, iteration)
+
+y_pred = np.dot(x_test, theta)
+print(y_pred)
+error = ( 1/x_test.shape[0] ) * np.sum(np.abs( y_pred - y_test ))
+print(error * 100)
+print((1 - error) * 100 )
 
 
--- README.md
+++ README.md
@@ -1,8 +1,45 @@
-# All_Classifier_from_Scratch
+# All Classifiers from Scratch
 
-List of all the Classifier:
-- Linear Regression
+This repository contains implementations of various machine learning classifiers from scratch using the Iris dataset.
+
+## Classifiers Implemented
+- Perceptron
 - Logistic Regression
-- Decision Tree
+- SGD Classifier
+- K-Nearest Neighbors (KNN)
+- Naive Bayes
+  - GaussianNB
+  - MultinomialNB
+  - ComplementNB
+  - BernoulliNB
+  - CategoricalNB
 - Random Forest
-- Support Vector Classifier(SVM)
+- Support Vector Machine (SVM)
+- Decision Tree
+- Gaussian Process
+- Multi-Layer Perceptron (MLP)
+
+## Getting Started
+
+### Prerequisites
+- Python 3.x
+- Required packages listed in `requirements.txt`
+
+### Installation
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/All_Classifier_from_Scratch.git
+    cd All_Classifier_from_Scratch
+    ```
+
+2. Install the required packages:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+### Usage
+To run any classifier, navigate to the respective file in `classifiers/` and execute it. For example:
+```bash
+python classifiers/perceptron.py
+```
+
